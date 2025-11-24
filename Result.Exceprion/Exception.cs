c961946bbbbde976089200c604b08f1d201db30b
Jason Pereira <jasonpereira84@gmail.com>
2025-11-23 19:38:50 -0600

    Added new ResultException project

---
+++ Result.Exceprion/Exception.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JasonPereira84.Result
+{
+    public class ResultException<TValue> : ResultException<Exception, TValue>
+    {
+        public ResultException(Nullable<Boolean> overall, Exception error, TValue value)
+            : base(overall, error, value)
+        {
+        }
+    }
+
+}
---
+++ Result.Exceprion/_Base.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JasonPereira84.Result
+{
+    public partial class ResultException<TException, TValue> : Exception
+        where TException : Exception
+    {
+        public ResultException(Nullable<Boolean> overall, TException error, TValue value)
+        {
+            Overall = overall;
+            Error = error;
+            Value = value;
+        }
+
+        public Nullable<Boolean> Overall { get; internal set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public TException Error { get; internal set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public TException Exception { get => Error; internal set => Error = value; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        public TValue Value { get; internal set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    }
+
+}
